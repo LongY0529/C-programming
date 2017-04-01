2f6feca9156152514bd8f6fe6c5579964ba632de
LongY0529 <LongY0529@icloud.com>
2017-04-02 00:47:19 +0800

    Update 9-8.c

--- C programming language fourth edition/9. Struct/9-8.c
+++ C programming language fourth edition/9. Struct/9-8.c
@@ -15,32 +15,32 @@ struct student *insert(struct student *head,struct student *stud)
  	p1 = head;	//point p1 to first node 
  	p0 = stud;	//point to the node needs to be inserted
  	if(head == NULL)	//if the likedlist is a null
-    {
-   	   head = p0;	//define point p0 point to the first node 
-	   p0 -> next = NULL;
+    	{
+   	   	head = p0;	//define point p0 point to the first node 
+	   	p0 -> next = NULL;
 	}	//end if 
-    else
-    {
-	   while((p0 -> num > p1 -> num) && (p1 -> next != NULL))
-       {
-	        p2=p1;	//point p2 to pointer p1
-            p1=p1->next;	//p1 back for one node 
-       }	//end while
-            if(p0 -> num <= p1->num)
-            {
+    	else
+    	{
+	   	while((p0 -> num > p1 -> num) && (p1 -> next != NULL))
+       		{
+	        	p2=p1;	//point p2 to pointer p1
+            		p1=p1->next;	//p1 back for one node 
+       		}	//end while
+            	if(p0 -> num <= p1->num)
+            	{
 	        	if(head == p1)	//insert at the front of the initial node 
-				   head = p0;         
-                else 
-	        	   p2 -> next = p0;	//insert at the node pointer2 points at
-            	   p0 -> next = p1;
-	       }	//end if
-           else
-	       {
+				head = p0;         
+                	else 
+	        	  	p2 -> next = p0;	//insert at the node pointer2 points at
+            	   		p0 -> next = p1;
+	       	}	//end if
+           	else
+	       	{
 		        p1 -> next = p0;	//insert at the front of the last node
-	            p0 -> next = NULL;
-           }	//end else
-   }
-    n = n + 1;	//the number of node + 1                        
-    return (head);
+	            	p0 -> next = NULL;
+           	}	//end else
+   	}
+    	n = n + 1;	//the number of node + 1                        
+    	return (head);
 }	//end student* insert()
 
