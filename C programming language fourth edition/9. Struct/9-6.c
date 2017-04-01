98a46fcfa9d89b274de21087b06f27fee7a5ace6
LongY0529 <LongY0529@icloud.com>
2017-04-02 00:48:19 +0800

    Update 9-6.c

--- C programming language fourth edition/9. Struct/9-6.c
+++ C programming language fourth edition/9. Struct/9-6.c
@@ -16,34 +16,34 @@ int main()
 {
  	int i,flag,h;
   	for (i = 1; i <= N; i++)	//for all 13 people in the circle 
-    {
-			if (i == N)	//if count number to the last people 
-				stu[i].flag = 1;	//set the flag as 1
+    	{
+		if (i == N)	//if count number to the last people 
+			stu[i].flag = 1;	//set the flag as 1
     		else
        			stu[i].flag = i + 1;	//if dont count to last people, set flag as i + 1
      			stu[i].number = i;	//set the number of this people is i
-    }	///end for 
+    	}	///end for 
   	printf("\n");
   	flag = 1;
   	h = N;
   	printf("people leave the circle:\n");	//output the number leave the circle 
   	while(flag < N-1)	//if dont count to the last people
-    {
-			i = 0;
+    	{
+		i = 0;
      		while(i != 3)	//if the number is not 3
-            {
+            	{
 	   		h = stu[h].flag;
-	    	i++;	//continue to count
-            }	//end while 
+	    		i++;	//continue to count
+            	}	//end while 
     		printf("%4d",stu[h].number);	//output the number 
     		stu[h].number = 0;	
      		flag++;
-    }	//end while 
+    	}	//end while 
   	printf("\nThe last student is ");
   	for (i = 1; i <= N; i++)	//output the result 
     		if (stu[i].number)
       			printf("%3d",stu[i].number);
-  			printf("\n");
+  		printf("\n");
   	return 0;
 }	//end main()
 
