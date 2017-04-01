ee66b1533f0f883c036e3600176293c9cd1690e9
LongY0529 <LongY0529@icloud.com>
2017-04-02 00:52:17 +0800

    Update 9-5.c

--- C programming language fourth edition/9. Struct/9-5.c
+++ C programming language fourth edition/9. Struct/9-5.c
@@ -18,39 +18,39 @@ int main()
  	FILE *fp;	//define a file pointer fp 
  	int i,j,n;
   	if ((fp = fopen("stud","r")) == NULL)	//if file 'stud' can not be opened 
-    {
-		 printf("error!\n");	//output error! 
-     	 exit(0);
-    }	//end if 
+    	{
+		printf("error!\n");	//output error! 
+     	 	exit(0);
+    	}	//end if 
   	printf("File 'stud': ");	//if file stud can be opened 
   	for (i = 0;fread(&st[i],sizeof(struct student),1,fp) != 0; i++)	//read data from file till the file ends(= 0)
-    {
-		 printf("\n%8s%8s",st[i].num,st[i].name);	//input num and name 
-     	 for (j = 0; j < 3; j++)	//each name includes 3 scores 
+    	{
+		printf("\n%8s%8s",st[i].num,st[i].name);	//input num and name 
+     	 	for (j = 0; j < 3; j++)	//each name includes 3 scores 
 	  		  printf("%8d",st[i].score[j]);	//input scores 
-    printf("%10.2f",st[i].ave);
-    }	//end for 
+    		printf("%10.2f",st[i].ave);
+    	}	//end for 
   	printf("\n");	//change line 
   	fclose(fp);	//close file 
- 	 n = i;
+ 	n = i;
   	for (i = 0; i < n; i++)	//sort score in ascending order(bubble sort)
-     	 for (j = i + 1; j < n; j++)
+     	 	for (j = i + 1; j < n; j++)
 			  if (st[i].ave < st[j].ave)
 	  		  {
 				temp = st[i];
 	   			st[i] = st[j];
 	   			st[j] = temp;
 	  		  }	//end if 
-  	printf("New order is:");
-  	fp = fopen("stu_sort","w");	//open and write file 
+  		printf("New order is:");
+  		fp = fopen("stu_sort","w");	//open and write file 
   	for (i = 0; i < n; i++)	//output the new order 
-    {
-	 	 fwrite(&st[i],sizeof(struct student),1,fp);	//wrtie data from st[i](length = sizeof(struct student) into file 
-     	 printf("\n%8s%8s",st[i].num,st[i].name);	//write num and name 
-      	 for (j = 0; j < 3; j++)
-	     	  printf ("%8d",st[i].score[j]);	//write scores 
-         printf("%10.2f",st[i].ave);
-     }	//end for 
+    	{
+	 	fwrite(&st[i],sizeof(struct student),1,fp);	//wrtie data from st[i](length = sizeof(struct student) into file 
+     	 	printf("\n%8s%8s",st[i].num,st[i].name);	//write num and name 
+      	 	for (j = 0; j < 3; j++)
+	     	  	printf ("%8d",st[i].score[j]);	//write scores 
+         	printf("%10.2f",st[i].ave);
+     	}	//end for 
   	printf("\n");
   	fclose(fp);	//close file 
   	return 0;
