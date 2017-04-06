d9833fb3f600909441e8421ab1a0732ad5012f90
LongY0529 <LongY0529@icloud.com>
2017-04-06 22:26:37 +0800

    Update 8-18.c

--- C programming language fourth edition/8. Pointer/8-18.c
+++ C programming language fourth edition/8. Pointer/8-18.c
@@ -2,16 +2,17 @@
 //define a function(use pointer)  whenever a integer is entered, return the month which is matched 
 #include <stdio.h>
 int main()
-{char *month[12]={"January","February","March","April",
-   "May","June","july","August","September","October", "November","December"};	//define the pointer month
-int n;
-printf("input a integer blow 12:\n");	//input the integer
-scanf("%d",&n);
-if ((n <= 12) && (n >= 1))	//judge wether the integer is legal( < 12)
-   printf("It is %s.\n",*( month + n - 1));	//if true, output the month
-else
-  printf("error!\n");	//if not, output error 
-return 0;
+{
+    char *month[12]={"January","February","March","April","May","June",
+                     "july","August","September","October", "November","December"};	//define the pointer month
+    int n;
+    printf("input a integer blow 12:\n");	//input the integer
+    scanf("%d",&n);
+    if ((n <= 12) && (n >= 1))	//judge wether the integer is legal( < 12)
+       printf("It is %s.\n",*( month + n - 1));	//if true, output the month
+    else
+       printf("error!\n");	//if not, output error 
+    return 0;
 }	//end main()
 
 
