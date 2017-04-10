2d3730685affc7cc7ec84b790e600c57d0c05c07
LongY0529 <LongY0529@icloud.com>
2017-04-10 22:01:12 +0800

    Update exercise 7.c

--- C programming language fourth edition/10. Ioput/exercise 7.c
+++ C programming language fourth edition/10. Ioput/exercise 7.c
@@ -24,32 +24,32 @@ int main()
   	scanf("%d,%d,%d",&s.score[0],&s.score[1],&s.score[2]);
   	s.ave=(s.score[0]+s.score[1]+s.score[2]) / 3;	//calculate the average score of student
   	if((fp = fopen("stu_sort","r")) == NULL)	//if file can not be opened
-    {
+    	{
 		printf("error!.");	//output error
-     	exit(0);
-    }	//end if 
+     		exit(0);
+    	}	//end if 
   	printf("students' data:\n");	//if the file can be opened
     	for (i = 0;fread(&st[i],sizeof(struct student),1,fp) != 0; i++)	//read all the data from struct i 
       	{
-		  	printf("\n%8s%8s",st[i].num,st[i].name);	//output the student's name and No
+		printf("\n%8s%8s",st[i].num,st[i].name);	//output the student's name and No
          	for (j = 0; j < 3; j++)
 	       		printf("%d",st[i].score[j]);	//output 3 scores of the student
        		printf("%f",st[i].ave);	//output the average score of student
       	}	//end for 
 	n = i;
   	for (t = 0;st[t].ave > s.ave && t < n; t++);
-  	printf("Now:\n");
-  	fp1 = fopen("sort data","w");	//open file sort data
+  		printf("Now:\n");
+  		fp1 = fopen("sort data","w");	//open file sort data
   	for (i = 0; i < t; i++)
-    {
+    	{
 		fwrite(&st[i],sizeof(struct student),1,fp1);	//read all the data from struct i
-     	printf("\n %s%s",st[i].num,st[i].name);	//output the student's name and No
-     	for (j = 0; j < 3; j++)
-       		printf("%d",st[i].score[j]);	//output 3 scores of student
-     	printf("%f",st[i].ave);
-    }	//end for 
+     		printf("\n %s%s",st[i].num,st[i].name);	//output the student's name and No
+     		for (j = 0; j < 3; j++)
+       			printf("%d",st[i].score[j]);	//output 3 scores of student
+     			printf("%f",st[i].ave);
+    	}	//end for 
   	fclose(fp);
   	fclose(fp1);
   	return 0;
- }	//end main()
+}	//end main()
 
