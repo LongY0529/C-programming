43fd33dba33928d3ccaa691413e47b877b2e69f2
LongY0529 <LongY0529@icloud.com>
2017-04-10 19:00:18 +0800

    Update and rename 10-10.c to exercise 10.c

--- C programming language fourth edition/10. Ioput/exercise 10.c
+++ C programming language fourth edition/10. Ioput/exercise 10.c
@@ -10,15 +10,15 @@ struct emploee2	//define struct emploee
 data2[20];
 
 int main()
- { 
+{ 
  	FILE *fp;	//define file pointer fp
    	int i,j,n,flag;
    	char name[10];	//define length of name
    	if ((fp = fopen("emploee2","rb")) == NULL)	//if file emploee2 can not be opened
-    {
+    	{
 		printf("error!\n");	//output error
-      	exit(0);
-    }	//end if
+      		exit(0);
+    	}	//end if
    	for (i = 0; fread(&data2[i],sizeof(struct emploee),1,fp) != 0; i++)	//if file can be opened 
       	printf(" %s%d",data2[i].name,data2[i].salary);	//output data from data2
    	fclose(fp);
@@ -26,35 +26,34 @@ int main()
    	printf("input name deleted:\n");	//input the emploee you want to delete
    	scanf("%s",name);	//scan the name
    	for (flag = 1, i = 0; flag && i < n; i++)	//search all the name in the data
-    {
+    	{
 		if (strcmp(name,data2[i].name)==0)	//if the name is not found
-        {
+        	{
 			for (j = i; j < n - 1; j++)
-            {
+            		{
 		   		strcpy(data2[j].name,data2[j+1].name);	//copy data 
 	        	data2[j].salary=data2[j+1].salary;
-           	}	//end for
+           		}	//end for
 	     	flag = 0;
-    	}	//end if
-    }	//end for
+    		}	//end if
+    	}	//end for
    	if(flag != 0)	//if the name is found
      	n = n - 1;	//the number of emploee - 1
    	else
      	printf("\ne!");	
-   	printf("\nNow,The content of file:\n");
-   	if((fp = fopen("emploee2","wb")) == NULL)
-    {
-		printf("can not open file\n");
-      	exit(0);
-    }	//end if
-   	for (i = 0; i < n; i++)
-      	fwrite(&data2[i],sizeof(struct emploee),1,fp);
+   	printf("new file is:\n");
+   	if((fp = fopen("emploee2","wb")) == NULL)	//if file emploee2 can not be opened
+    	{
+		printf("error!\n");	//output error
+      		exit(0);
+    	}	//end if
+   	for (i = 0; i < n; i++)	//if file can be opened
+      	fwrite(&data2[i],sizeof(struct emploee),1,fp);	//input data into data2
    	fclose(fp);
-   	fp=fopen("emploee2","r");
-   	for (i = 0;fread(&data2[i],sizeof(struct emploee),1,fp) != 0; i++)
-      	printf("\n%8s   %7d",data2[i].name,data2[i].salary);
-   	printf("\n");
-   	fclose(fp);
+   	fp=fopen("emploee2","r");	//open file emploee2
+   	for (i = 0;fread(&data2[i],sizeof(struct emploee),1,fp) != 0; i++)	//read data in data2	
+      		printf("\n%8s   %7d",data2[i].name,data2[i].salary);
+   	fclose(fp);	//change line
    	return 0;
- }	//end main()
+}	//end main()
 
