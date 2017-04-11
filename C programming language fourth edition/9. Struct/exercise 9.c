d898f8b911ff1a25a06ef01bc4923364180ccc51
LongY0529 <LongY0529@icloud.com>
2017-04-11 23:36:45 +0800

    Update and rename 9-9.c to exercise 9.c

--- C programming language fourth edition/9. Struct/exercise 9.c
+++ C programming language fourth edition/9. Struct/exercise 9.c
@@ -40,36 +40,37 @@ struct student *input()
     while(p1->num != 0) //if the num p1 points to do not equal to 0
     {
         n=n+1;  //the numebr + 1
-	    if(n == 1)  //when n = 1  
+	if(n == 1)  //when n = 1  
             head = p1;  //define p1 as the head address 
-	    else    //when != 1
+	else    //when != 1
             p2->next=p1;    //assign p1 to the next node of p1
 	    p1=(struct student*)malloc(LEN);    //p1 points to the addresss stored struct
 	    scanf("%ld,%f",&p1->num,&p1->score);
-	}   //end while
+    }   //end while
     p2->next = NULL;
     return(head);
 }   //end input()
 
 struct student *delete(struct student *head,long num)
 {
- 	struct student *p1,*p2;
-  	if (head == NULL)	//if the head address of chain is null
+    struct student *p1,*p2;
+    if (head == NULL)	//if the head address of chain is null
     {
 			printf("error!\n");	//output error
-      return(head);
+    return(head);
     }	//end if 
-  	p1=head;	//p1 point to the first node
-  	while (num != p1->num && p1->next != NULL)	//if the ndoe of p1 is not the node we want 
-		  p2 = p1;	//assign p1 to p2
-		  p1 = p1->next;	//assian the next node of p1 to p1
-		} //end while               
-  	if (num == p1->num) //if the next value of p1 is the node we want                    
+    p1=head;	//p1 point to the first node
+    while (num != p1->num && p1->next != NULL)	//if the ndoe of p1 is not the node we want 
+    }	  
+	    p2 = p1;	//assign p1 to p2
+	    p1 = p1->next;	//assian the next node of p1 to p1
+    } //end while               
+    if (num == p1->num) //if the next value of p1 is the node we want                    
     {	
-			if(p1 == head)	//if the value of head address equal to p1
-		   	head = p1->next;	//define head as the second node 
+	    if(p1 == head)	//if the value of head address equal to p1
+		head = p1->next;	//define head as the second node 
 	    else 
-		   	p2->next = p1->next; 	//if not, assign next node of p1 as next node of p2 
+		p2->next = p1->next; 	//if not, assign next node of p1 as next node of p2 
 	    	n = n - 1;	//the number of node - 1
     }//end if 
     else printf("%ld not been found!\n",num);	//cant find the node  
@@ -79,28 +80,28 @@ struct student *delete(struct student *head,long num)
 
 struct student *insert(struct student *head,struct student *stud)
 {
-	struct student *p0,*p1,*p2;
- 	p1 = head;	//point p1 to first node 
- 	p0 = stud;	//point to the node needs to be inserted
- 	if(head == NULL)	//if the likedlist is a null
-    	{
-   	   	head = p0;	//define point p0 point to the first node 
-	   	p0 -> next = NULL;
-	    }	//end if 
-    	else
-    	{
-	   	while((p0 -> num > p1 -> num) && (p1 -> next != NULL))
-       		{
-	        	p2=p1;	//point p2 to pointer p1
-            		p1=p1->next;	//p1 back for one node 
-       		}	//end while
-            	if(p0 -> num <= p1->num)
-            	{
-	        	if(head == p1)	//insert at the front of the initial node 
-				head = p0;         
-                	else 
-	        	  	p2 -> next = p0;	//insert at the node pointer2 points at
-            	   		p0 -> next = p1;
+    struct student *p0,*p1,*p2;
+    p1 = head;	//point p1 to first node 
+    p0 = stud;	//point to the node needs to be inserted
+    if(head == NULL)	//if the likedlist is a null
+    {
+   	   head = p0;	//define point p0 point to the first node 
+	   p0 -> next = NULL;
+    }	//end if 
+    else
+    {
+	   while((p0 -> num > p1 -> num) && (p1 -> next != NULL))
+       	   {
+	        p2=p1;	//point p2 to pointer p1
+            	p1=p1->next;	//p1 back for one node 
+       	   }	//end while
+           if(p0 -> num <= p1->num)
+           {
+	        if(head == p1)	//insert at the front of the initial node 
+			head = p0;         
+                else 
+	        	p2 -> next = p0;	//insert at the node pointer2 points at
+            	   	p0 -> next = p1;
 	       	}	//end if
            	else
 	       	{
@@ -115,13 +116,13 @@ struct student *insert(struct student *head,struct student *stud)
 void output(struct student *head)
 {
     struct student *p;
-	printf("new data is:\n",n); //output the new data  
-	p=head; //pointer p points to the head address of data
-	if(head != NULL)    //if the value of head is not null
-	   do
-	   {
-           printf("%ld %5.1f\n",p->num,p->score);   //print the data
+    printf("new data is:\n",n); //output the new data  
+    p=head; //pointer p points to the head address of data
+    if(head != NULL)    //if the value of head is not null
+	 do
+	 {
+               printf("%ld %5.1f\n",p->num,p->score);   //print the data
 	       p=p->next;   //p points to the next node
-	   }
-       while(p != NULL);    //till the value p points to is null
+	 }
+       	 while(p != NULL);    //till the value p points to is null
 }   //end output
