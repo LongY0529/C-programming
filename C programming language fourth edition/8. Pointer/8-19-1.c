ee6cf68d0ba48369daee60d50d87c665a0e3699f
LongY0529 <LongY0529@icloud.com>
2017-04-12 20:46:55 +0800

    Update 8-19-1.c

--- C programming language fourth edition/8. Pointer/8-19-1.c
+++ C programming language fourth edition/8. Pointer/8-19-1.c
@@ -7,4 +7,4 @@ char data[NEWSIZE]; //define struct data
 char *p=data; //define the pointer points to the head of the data           
 char *new(int n)  //declare function new
     p += n; //pointer p points to the end of the data
-	  return(p-n);  //return a pointer points to the head of the memory
+    return(p-n);  //return a pointer points to the head of the memory
